ebabff53f345059b8d3550877c441afd49d308f2
Ulises688apaza <ulisesapaza.280@gmail.com>
2021-05-24 11:48:12 -0300

    add some prints cool

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from colorama import Fore, init
 
 # Global Var
 strPDF = ""
+engine = ""
 
 # Get input from User
 def gInUs():
+
     global strPDF
     fileObj = ""
+
     # Print input
     print(Fore.GREEN + "[!] Insert path to PDF file:" + Fore.RESET)
     inputUser = input()
@@ -27,20 +30,29 @@ def gInUs():
 def reDoc(strFile):
     # Global var
     global strPDF
+    # -------------
 
     # Extract text
     fileObj = fitz.open(strFile)
     for page in fileObj:
         strPDF += page.get_text("text")
+    print(Fore.GREEN + "[!] Init engine text to speech pyttsx3"+ Fore.RESET)
     reStr(strPDF)
     # -------------
 
 # Fun reStr that read the text from string strPDF
 def reStr(strRead):
+    global engine
+
+    # Init engine
     engine = pyttsx3.init()
-
     engine.say(strRead)
-    engine.runAndWait()
-
+    # -------------
+    print(Fore.YELLOW + "[.] Reading"+ Fore.RESET)
+    
 # Call to main fun
 gInUs()
+
+# Finish engine
+engine.runAndWait()
+print(Fore.GREEN + "[!] Finished"+ Fore.RESET)
